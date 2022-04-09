55a9aad56a1a729dfb4c3bd45718b1853c25345c
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2022-04-09 17:14:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(cors())
 
 app.use("/products", require('./routes/products.routes'))
 app.use("/foods", require('./routes/foods.routes'))
-router.use((req, res) => res.status(404).send({ code: 404, message: 'This route does not exist' }))
+app.use((req, res) => res.status(404).send({ code: 404, message: 'This route does not exist' }))
 
 const PORT = process.env.PORT || 5005
 app.listen(PORT, () => console.log("Server now started at: http://localhost:5005"))
