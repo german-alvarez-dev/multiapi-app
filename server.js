77b9537ae62a403c5a9e3f1910ab1ac3c6182bfd
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2022-04-12 19:17:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(cors())
 
 app.use("/products", require('./routes/products.routes'))
 app.use("/foods", require('./routes/foods.routes'))
+app.use("/users", require('./routes/users.routes'))
 
 const PORT = process.env.PORT || 5005
 app.listen(PORT, () => console.log("Server now started at: http://localhost:" , PORT))
